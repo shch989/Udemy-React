07e1b926b43efca29acea8102b87eefab8b04f69
shch989 <tankpop11@gmail.com>
2022-11-28 23:02:15 +0900

    2022-11-28

--- ch2/src/App.js
+++ ch2/src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from './components/ExpenseItem'
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense"
 
 function App() {
+
   const expenses = [
     {
       id: 'e1',
@@ -28,29 +30,15 @@ function App() {
     },
   ]
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem 
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem 
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem 
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem 
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />      
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses}/>
     </div>
   )
 }
