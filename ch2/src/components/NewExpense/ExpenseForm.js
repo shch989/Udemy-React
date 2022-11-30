7da330d9be2f0d5217a4ac0beec72b0645e75151
shch989 <tankpop11@gmail.com>
2022-11-30 20:49:51 +0900

    2022-11-30

--- ch2/src/components/NewExpense/ExpenseForm.js
+++ ch2/src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ function ExpenseForm(props) {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     }
 
@@ -87,6 +87,7 @@ function ExpenseForm(props) {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
