15bdddd7f82182fe2f98420ac7d8bcbaa1e730e9
shch989 <tankpop11@gmail.com>
2022-12-06 16:47:01 +0900

    2022-12-06

--- ch6/src/components/Meals/MealItem/MealItemForm.js
+++ ch6/src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import Input from '../../UI/Input'
 import classes from './MealItemForm.module.css'
 
 function MealItemForm(props) {
+  const amountInputRef = useRef()
+
+  const submitHandler = event => {
+    event.preventDefault();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: 'amount_' + props.id,
